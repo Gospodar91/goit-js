6d5d59c35e62922823791d40c72102b0762889db
Sergei <xsergeix@ukr.net>
2020-01-20 21:57:35 +0300

    fifth task

--- module-2/js/task-5.js
+++ module-2/js/task-5.js
@@ -1,34 +1,13 @@
-'use strict';
-let country = prompt("Введите страну");
-let price;
-const inLowerCaseCountry = country.toLowerCase();
-switch (inLowerCaseCountry) {
-    case "китай":
-        price = 100;
-        console.log(`Доставка в ${inLowerCaseCountry[0].toUpperCase()}${inLowerCaseCountry.substr(1,1000)} будет стоить ${price} кредитов`);
-        break;
-    case "чили":
-        price = 250;
-        console.log(`Доставка в ${inLowerCaseCountry[0].toUpperCase()}${inLowerCaseCountry.substr(1,1000)} будет стоить ${price} кредитов`);
-break;
-    case "австрия":
-        price = 170;
-        console.log(`Доставка в ${inLowerCaseCountry[0].toUpperCase()}${inLowerCaseCountry.substr(1,1000)} будет стоить ${price} кредитов`);
-        break;
-    case "индия":
-        price = 80;
-        console.log(`Доставка в ${inLowerCaseCountry[0].toUpperCase()}${inLowerCaseCountry.substr(1,1000)} будет стоить ${price} кредитов`);
-        break;
-    case "ямайка":
-        price = 120;
-        console.log(`Доставка в ${inLowerCaseCountry[0].toUpperCase()}${inLowerCaseCountry.substr(1,1000)} будет стоить ${price} кредитов`);
-        break;
-    default:
-        console.log('В вашей стране доставка не доступна')
-
-
-
-
-}
-
-
+"use strict";
+const checkForSpam = function(message) {
+  message = message.toLowerCase();
+  if (message.includes("sale") || message.includes("spam")) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(checkForSpam("Latest technology news")); //false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
