ef0da52a6134aa815de62446f8739b59056fb592
Sergei <xsergeix@ukr.net>
2020-01-21 13:01:42 +0300

    new fix

--- module-2/js/task-6.js
+++ module-2/js/task-6.js
@@ -1,16 +1,32 @@
 'use strict';
-let input;
+let input= prompt("Введите число");
 const numbers = [];
 let total = 0;
 while (input !== null) {
-  input = prompt("Введите число");
-  if (Number.isNaN(Number(input))) {
+  if (isNaN(input)) {
     alert("Было введено не число,попробуйте еще раз")
+    input = prompt("Введите число");
+    continue;
   } else {
     numbers.push(input);
   }
+  input = prompt("Введите число");
 }
+
+
 for (let i = 0; i < numbers.length; i++) {
   total += Number(numbers[i])
 }
 alert(`Общая сумма чисел равна ${total}`);
+
+// while (input !== null) {
+//   if (isNaN(input)) {
+//     alert("Было введено не число,попробуйте еще раз");
+//     input = prompt("Введите число");
+//     continue;
+//   } else {
+//     total = total + Number(input);
+//   }
+//   input = prompt("Введите число");
+// }
+// alert(`Общая сумма чисел равна ${total}`);
