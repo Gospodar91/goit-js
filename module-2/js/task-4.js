d80e851f10b892751fc226086a56db8b65fd8174
Sergei <xsergeix@ukr.net>
2020-01-21 15:25:42 +0300

    fix

--- module-2/js/task-4.js
+++ module-2/js/task-4.js
@@ -1,24 +1,16 @@
 'use strict';
-// const formatString = function (string) {
-//     string = string.split('')
-//     if (string.length >= 40) {
-//         string = string.splice(0, 40);
-//         string = string.join(''); // обьеденяет массив ,заменяя запятые на пробел 
-//         return string + "...";
-//     } else {
-//         string = string.join(''); // обьеденяет массив ,заменяя запятые на пробел 
-//         return string;
-//     }
-// };
 const formatString = function (string) {
-    if (string.length > 40) {
-        const del = string.length - 40;
-        string = string.substring(del);
+    string = string.split('')
+    if (string.length >= 40) {
+        string = string.splice(0, 40);
+        string = string.join(''); // обьеденяет массив ,заменяя запятые на пробел 
         return string + "...";
     } else {
+        string = string.join(''); // обьеденяет массив ,заменяя запятые на пробел 
         return string;
     }
-}
+};
+
 console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
 console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
 console.log(formatString('Curabitur ligula sapien.'));
