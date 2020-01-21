b73f0065911a77342250970f460505e6df93cb18
Sergei <xsergeix@ukr.net>
2020-01-21 14:06:03 +0300

    new fix

--- module-2/js/task-1.js
+++ module-2/js/task-1.js
@@ -1,7 +1,7 @@
 'use strict';
 const logItems = function (array) {
     for (let i = 0; i < array.length; i++) {
-        console.log(`${i+1} - ${array[i]}`)
+        console.log(`${i++} - ${array[i]}`)
     }
 };
 logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
--- module-2/js/task-6.js
+++ module-2/js/task-6.js
@@ -19,14 +19,3 @@ for (let i = 0; i < numbers.length; i++) {
 }
 alert(`Общая сумма чисел равна ${total}`);
 
-// while (input !== null) {
-//   if (isNaN(input)) {
-//     alert("Было введено не число,попробуйте еще раз");
-//     input = prompt("Введите число");
-//     continue;
-//   } else {
-//     total = total + Number(input);
-//   }
-//   input = prompt("Введите число");
-// }
-// alert(`Общая сумма чисел равна ${total}`);
