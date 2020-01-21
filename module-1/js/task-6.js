777fe63bc92b5a52492dfe9094be4fe9323613d3
Sergei <xsergeix@ukr.net>
2020-01-21 12:43:35 +0300

    new fix

--- module-1/js/task-6.js
+++ module-1/js/task-6.js
@@ -2,11 +2,13 @@
 let input = prompt("Введите число");
 let total = 0;
 while (input !== null) {
-  input = prompt("Введите число");
-  if (Number.isNaN(Number(input))) {
-    alert("Было введено не число,попробуйте еще раз")
+  if (isNaN(input)) {
+    alert("Было введено не число,попробуйте еще раз");
+    input = prompt("Введите число");
+    continue;
   } else {
     total = total + Number(input);
   }
+  input = prompt("Введите число");
 }
 alert(`Общая сумма чисел равна ${total}`);
