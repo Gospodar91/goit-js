c00ee7caf2387e7f7b05bce69bef75a42ed09d88
Sergei <xsergeix@ukr.net>
2020-01-28 13:00:03 +0300

    fix

--- module-5/js/task-4.js
+++ module-5/js/task-4.js
@@ -18,12 +18,16 @@ class StringBuilder {
 
   append(str) {
     this._value = this._value + str;
+    return this._value;
   }
   prepend(str) {
     this._value = str + this._value;
+    return this._value;
   }
   pad(str) {
     this._value = str + this._value + str;
+    return this._value;
+    
   }
 }
 const builder = new StringBuilder(".");
