9b3587e489d8f2434f0488197be1293ad9584eaa
Sergei <xsergeix@ukr.net>
2020-02-10 23:34:07 +0300

    first

--- goit-js-hw-10-food-service/src/index.js
+++ goit-js-hw-10-food-service/src/index.js
@@ -1,1 +1,41 @@
 import './styles.css';
+import mainArray from './menu-jason.json';
+import foodList from './menu.hbs';
+const refs = {
+  mainRef: document.querySelector('.js-menu'),
+  bodyRef: document.querySelector('body'),
+  buttonSelector: document.querySelector('.js-switch-input'),
+};
+const markup = foodList(mainArray);
+refs.mainRef.insertAdjacentHTML('afterbegin', markup);
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+function defaultTheme() {
+  // проверка начальной темы при загрузки страницы
+  if (localStorage.getItem('theme') === Theme.LIGHT) {
+    refs.bodyRef.classList.add(Theme.LIGHT);
+    refs.buttonSelector.checked = false;
+  } else if (localStorage.getItem('theme') === Theme.DARK) {
+    refs.bodyRef.classList.add(Theme.DARK);
+    refs.buttonSelector.checked = true;
+  } else {
+    localStorage.setItem('theme', Theme.LIGHT);
+    refs.bodyRef.classList.add(Theme.LIGHT);
+  }
+}
+defaultTheme();
+refs.bodyRef.addEventListener('click', onClick);
+function onClick(e) {
+  //Проверка true/false  кнопки
+  if (refs.buttonSelector.checked) {
+    refs.bodyRef.classList.remove(Theme.LIGHT);
+    refs.bodyRef.classList.add(Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    refs.bodyRef.classList.remove(Theme.DARK);
+    refs.bodyRef.classList.add(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
---
+++ goit-js-hw-10-food-service/src/task1.js
@@ -0,0 +1,16 @@
+
+
+// import mainArray from './menu-jason.json';
+// import hbsVar from './menu.hbs';
+
+//  const qwe=document.querySelector('.menu');
+//  qwe.insertAdjacentHTML("afterbegin", hbsVar);
+
+// function arrSplit (array){
+//   return array.forEach(element => {
+//    console.log(element);
+    
+//   });
+// }
+// console.log(arrSplit(mainArray));
+// // console.log(mainArray);
