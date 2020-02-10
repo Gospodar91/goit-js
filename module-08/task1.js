51f717175583a3099a0f98ca34f514a0874bf99b
Sergei <xsergeix@ukr.net>
2020-02-10 10:07:13 +0300

    fix

---
+++ module-08/task1.js
@@ -0,0 +1,58 @@
+"use strict";
+
+import galleryImages from "./gallery-items.js";
+
+const galleryContainer = document.querySelector(".js-gallery");
+const gelleryLayout = generateGalleryLayout(galleryImages);
+const lightboxImg = document.querySelector(".lightbox__image");
+const lightbox = document.querySelector(".js-lightbox");
+
+function generateGalleryLayout(array) {
+  return array
+    .map(
+      image => `<li class="gallery__item">
+    <a
+    class="gallery__link"
+    href="#">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+    </li>`
+    )
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("afterbegin", gelleryLayout);
+
+galleryContainer.addEventListener("click", openFullSize);
+
+function openFullSize(e) {
+  if (e.target.classList.contains("gallery__image")) {
+    const fullImg = e.target;
+    lightbox.classList.add("is-open");
+    lightboxImg.src = fullImg.dataset.source;
+    lightboxImg.alt = fullImg.alt;
+    lightbox.addEventListener("click", onClose);
+    galleryContainer.addEventListener("keydown", onEscape);
+  }
+}
+function onClose(e) {
+  if (!e.target.classList.contains("gallery__image")) {
+    lightbox.classList.remove("is-open");
+    lightbox.removeEventListener("click", onClose);
+    lightbox.removeEventListener("keydown", onEscape);
+  }
+}
+
+function onEscape(e) {
+  if (e.code === "Escape") {
+    lightbox.classList.remove("is-open");
+    lightbox.removeEventListener("click", onClose);
+    galleryContainer.removeEventListener("keydown", onEscape);
+  }
+}
+
